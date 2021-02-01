82f254865b243cc54d6a0d2d9e3773785d92bd23
Maximotus <erlermax@web.de>
2021-02-01 13:08:11 +0100

    Added empty line between docstring and first method of class

--- tkge/common/error.py
+++ tkge/common/error.py
@@ -1,5 +1,6 @@
 class ConfigurationError(Exception):
     """Exception for error cases in the `tkge.common.config.Config` class."""
+
     def __init__(self, msg: str):
         super().__init__()
         self.msg = msg
