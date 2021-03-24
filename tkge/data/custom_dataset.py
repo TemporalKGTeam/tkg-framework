0a448a6d6b73b11dea0e3ad4a98ff492602506c7
Maximotus <erlermax@web.de>
2021-03-24 15:05:03 +0100

    Copied process and process_time functions in new created class YAGO15kDatasetProcessor and adapted the implementation to fit the sequence encoding.

--- tkge/data/custom_dataset.py
+++ tkge/data/custom_dataset.py
@@ -108,6 +108,116 @@ class Yago11kDatasetProcessor(DatasetProcessor):
         return year
 
 
+@DatasetProcessor.register(name="yago15k_TA")
+class YAGO15KDatasetProcessor(DatasetProcessor):
+    def process(self):
+        """
+        Processes the raw data for each data type (i.e. train, valid and test) of the YAGO15k dataset.
+        Uses the resulting timespan of the function process_time to add as many quadruples to each data set as years
+        where that fact is true.
+        """
+        self.tem_dict = {
+            '0y': 0, '1y': 1, '2y': 2, '3y': 3, '4y': 4, '5y': 5, '6y': 6, '7y': 7, '8y': 8, '9y': 9,
+            '01m': 10, '02m': 11, '03m': 12, '04m': 13, '05m': 14, '06m': 15, '07m': 16, '08m': 17, '09m': 18,
+            '10m': 19, '11m': 20, '12m': 21,
+            '0d': 22, '1d': 23, '2d': 24, '3d': 25, '4d': 26, '5d': 27, '6d': 28, '7d': 29, '8d': 30, '9d': 31,
+        }
+
+        data_splits = ["train", "valid", "test"]
+        data_raw_mappings = {data_splits[0]: self.train_raw, data_splits[1]: self.valid_raw,
+                             data_splits[2]: self.test_raw}
+        data_set_mappings = {data_splits[0]: self.train_set, data_splits[1]: self.valid_set,
+                             data_splits[2]: self.test_set}
+
+        for data_split in data_splits:
+            index = 0
+            while index < len(data_raw_mappings[data_split]):
+                rd = data_raw_mappings[data_split][index]
+                fact = rd.strip().split('\t')
+                # self.config.log(f"Processing fact in line {index + 1}: {fact}")
+
+                triple = fact[0][1:-1], fact[1][1:-1], fact[2][1:-1]
+                head_id = self.index_entities(triple[0])
+                rel_id = self.index_relations(triple[1])
+                tail_id = self.index_entities(triple[2])
+
+                year_start, year_stop, index = self.process_time(data_split, index=index, fact=fact, triple=triple)
+
+                for y in range(year_start, year_stop + 1):
+                    ts_id = self.index_timestamps(y)
+                    ts_float = []
+                    ts_float.extend([self.tem_dict[f'{int(yi):01}y'] for yi in str(y)])
+
+                    data_set_mappings[data_split]['triple'].append([head_id, rel_id, tail_id])
+                    data_set_mappings[data_split]['timestamp_id'].append([ts_id])
+                    data_set_mappings[data_split]['timestamp_float'].append(ts_float)
+
+                    self.all_triples.append([head_id, rel_id, tail_id])
+                    self.all_quadruples.append([head_id, rel_id, tail_id, ts_id])
+
+                    # self.config.log(f" {data_split} tuple: {str([head_id, rel_id, tail_id, ts_id, ts_float])}")
+
+                index += 1
+
+    def process_time(self, origin: str, index: int = 0, fact: List[str] = None, triple: Tuple = None):
+        """
+        Processes the time information in the YAGO15k dataset.
+        Since there are not only timestamps but also time modifiers, namely 'occursSince' and 'occursUntil', and
+        corresponding tuples that appear twice in a row, a few cases need to be addressed:
+        - Case 1: The timespan is given by the data.
+        - Case 2: The timespan is not given explicitly, but with argument-less modifiers that indicate universal truth.
+        - Case 3: The timespan is partly given by the data, i.e. the start time is known.
+        - Case 4: The timespan is partly given by the data, i.e. the end time is known.
+        - Case 5: There is no time information given at all.
+        In cases 1 and 2 the index needs to be incremented by 1 because the next line was already processed then.
+        Returns the start and end timestamp (only as the year) of the triple, as well as the maybe modified index.
+        """
+        data = {"train": self.train_raw, "valid": self.valid_raw, "test": self.test_raw}
+        data_raw = data[origin]
+        valid_temp_mods = ["occursSince", "occursUntil"]
+
+        # process time of the current fact
+        temp_mod = fact[3][1:-1] if len(fact) >= 4 and fact[3][1:-1] in valid_temp_mods else None
+        year = int(fact[4].split('-')[0][1:]) if len(fact) == 5 else 0
+
+        # process time of the next fact only if there exists at least one more fact after this
+        if index + 1 < len(data_raw):
+            next_fact = data_raw[index + 1].strip().split('\t')
+            next_triple = next_fact[0][1:-1], next_fact[1][1:-1], next_fact[2][1:-1]
+
+            next_temp_mod = next_fact[3][1:-1] if len(next_fact) >= 4 and next_fact[3][1:-1] in valid_temp_mods else None
+            next_year = int(next_fact[4][1:].split('-')[0]) if len(next_fact) == 5 else 0
+
+            is_closed_timespan = triple == next_triple and temp_mod and year != 0 and next_temp_mod and next_year != 0
+        else:
+            next_temp_mod = None
+            next_year = 0
+
+            is_closed_timespan = False
+
+        if is_closed_timespan:
+            # case 1: same triple appears twice in a row and has a temporal modifier as well as a timestamp
+            year_stop = next_year
+            index += 1
+        elif temp_mod and next_temp_mod and year == 0 and next_year == 0:
+            # case 2: same triple appears twice in a row but has only a temporal modifier and no timestamp
+            year = int(self.config.get('dataset.args.year_min'))
+            year_stop = int(self.config.get('dataset.args.year_max'))
+            index += 1
+        elif temp_mod and temp_mod == 'occursSince':
+            # case 3: triple appears only once and is true until now (or forever from the start year)
+            year_stop = int(self.config.get('dataset.args.year_max'))
+        elif temp_mod and temp_mod == 'occursUntil':
+            # case 4: triple appears only once and was true until a certain year
+            year_stop = year
+            year = int(self.config.get('dataset.args.year_min'))
+        else:
+            # case 5: otherwise there is no timespan, i.e. a stand alone triple
+            year_stop = year
+
+        return year, year_stop, index
+
+
 @DatasetProcessor.register(name="icews14_TA")
 class ICEWS14TADatasetProcessor(DatasetProcessor):
     def process(self):
@@ -162,8 +272,6 @@ class ICEWS14TADatasetProcessor(DatasetProcessor):
 
             self.all_triples.append([head, rel, tail])
             self.all_quadruples.append([head, rel, tail, ts_id])
-
-
 
     def process_time(self, origin: str):
         ts = []
