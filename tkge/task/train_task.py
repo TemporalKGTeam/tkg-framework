7e15853df9164e2fd37a6f0f37ca341ffa346d0d
GengyuanMax <gengyuanmax@gmail.com>
2021-04-23 13:38:23 +0200

    fix some bugs in train_task.py and resume_task.py: factors

--- tkge/task/train_task.py
+++ tkge/task/train_task.py
@@ -287,6 +287,7 @@ class TrainTask(Task):
 
         scores, factors = self.model.fit(samples)
 
+        factors = {} if factors is None
         self.config.assert_true(scores.size(0) == labels.size(
             0), f"Score's size {scores.shape} should match label's size {labels.shape}")
         loss = self.loss(scores, labels)
